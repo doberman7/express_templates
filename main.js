7b811e6923477ef73fdddbfa6858a27860a94f73
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-08-12 18:05:52 -0500

    10.2. passing data from controllers

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-const port = 3000
+"use strict";
+
+const port = 3000,
   express = require ("express"),
   app = express(),
-  chalkAnimation = require('chalk-Animation')
-  homeController = require("./controllers/homeController.js")
+  homeController = require("./controllers/homeController.js"),
+  chalkAnimation = require('chalk-Animation');
 
-//use for template ejs
-app.set("view engine", "ejs")
 
 //set port to the environment variable..
 //..PORT value or 3000 if the former value is undefined
 app.set("port", process.env.PORT || 3000);
 
+//use for template ejs
+app.set("view engine", "ejs");
+
 //This route runs when a request is made to the /name path; then it calls the respondWithName function in the home controller
-respondWithName: app.get("/name", homeController.respondWithName);
+app.get("/name/:myName", homeController.respondWithName);
 
 app.listen(port ,() => {
-  chalkAnimation.rainbow(`Server running at http://localhost:${ app.get("port") }`)
+  chalkAnimation.rainbow(`Server running at http://localhost:${ app.get("port") }`);
 });
