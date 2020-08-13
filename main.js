9fb21e7039d8d658841ccfdbf2e062490cde5f9a
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-08-13 13:14:35 -0500

    habdling errors

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ const port = 3000,
   express = require ("express"),
   app = express(),
   homeController = require("./controllers/homeController.js"),
-  chalkAnimation = require('chalk-Animation');
-
+  chalkAnimation = require('chalk-Animation'),
+  errorController = require("./controllers/errorController.js"),
+  layouts = require("express-ejs-layouts");
 
 //set port to the environment variable..
 //..PORT value or 3000 if the former value is undefined
@@ -17,7 +18,9 @@ app.set("view engine", "ejs");
 //This route runs when a request is made to the /name path; then it calls the respondWithName function in the home controller
 app.get("/name/:myName", homeController.respondWithName);
 
+app.use(layouts);
 
+app.use(errorController.logErrors);
 
 
 app.listen(port ,() => {
